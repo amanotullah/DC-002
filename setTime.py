1c610d55bf2b5e50385d318673f685dbf9b321be
Joey Hagedorn <joey@joeyhagedorn.com>
2022-12-20 17:36:48 -0800

    Update setTime.py for Python3.

--- setTime.py
+++ setTime.py
@@ -1,22 +1,23 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import time
 try:
     import serial
 except:
-    print "Run the following command to install pyserial:\nsudo easy_install -U pyserial"
+    print("Run the following command to install pyserial:\nsudo easy_install -U pyserial")
 
 
 def getTime():
     return int(time.time()) - time.timezone
 
-print "Setting Time..."
-ser = serial.Serial('/dev/tty.usbmodem12341', 9600)
+print("Setting Time...")
+ser = serial.Serial('/dev/tty.usbmodem123451', 9600)
 initialTime = getTime()
 while (initialTime == getTime()):
     1+1
     #spin
 
-success = ser.write('T'+str(getTime()))
+bytestring = 'T{}'.format(getTime())
+success = ser.write(bytestring.encode('utf_8'))
 ser.flush()
 ser.close()
